42d4f08b2531e2117a7915451c694fbd8f1270ef
Jerry Relunia <jrelunia@team.com>
2016-01-13 15:13:07 -0500

    add process.env.PORT  in app js file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = require("./server/routes");
 
-// Start the server
-var server = app.listen(8000, function() {
+// Start the server  //switch between process.env.PORT || port
+var server = app.listen(process.env.PORT || 8000, function() {
  console.log('Listening on port %d', server.address().port);
 });
